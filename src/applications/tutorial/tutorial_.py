3ef86547b07959e3ed6db9cb53bd807db81904b1
IffyKech <iffykech@gmail.com>
2020-07-17 00:46:11 +0100

    Directory updated for assests

--- src/applications/tutorial/tutorial_.py
+++ src/applications/tutorial/tutorial_.py
@@ -29,14 +29,21 @@ class TutorialWindow:
 class Notebook2:
     def __init__(self, parent):
         self.parent = parent
-
-        # top frame and attributes
         self.top_frame = tkinter.Frame(parent, side="top", fill="x")
+        self.bottom_frame = tkinter.Frame(parent, side="top", fill="x")
+
+        # top frame attributes
         self.step1_lbl = tkinter.Label(self.top_frame, text="1.Open Inspect Element via the keyboard shortcut: 'F12' or"
                                                             "'Ctrl + Shift + I'")
 
+        # bottom frame attributes
+        self.step2_lbl = tkinter.Label(self.bottom_frame, text="2.Locate the 'Elements' tab at the top of the sidebar"
+                                                               "window")
+        self.bottom_img_frame = tkinter.Frame(self.bottom_frame, side="bottom")
 
-        self.bottom_frame = tkinter.Frame(parent, side="top", fill="x")
+
+
+
 
 
 
