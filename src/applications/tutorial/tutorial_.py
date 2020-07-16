48acb3aa891d6a8df542b00121d2bb67a973a6af
IffyKech <iffykech@gmail.com>
2020-07-17 00:33:19 +0100

    Inital code written for tutorial window

--- src/applications/tutorial/tutorial_.py
+++ src/applications/tutorial/tutorial_.py
@@ -2,3 +2,42 @@ import tkinter
 from tkinter import messagebox
 from tkinter import ttk
 
+class TutorialWindow:
+    def __init__(self, app):
+        self.app = app
+        self.notebook_frame = tkinter.Frame(self.app)
+        self.notebook_frame.pack(fill="both", expand=1)
+
+        # create an instance of the Notebook
+        self.notebook = ttk.Notebook()
+
+        # different pages
+        self.frame0 = ttk.Frame(self.notebook)
+        self.frame1 = ttk.Frame(self.notebook)
+        self.frame2 = ttk.Frame(self.notebook)
+        self.frame3 = ttk.Frame(self.notebook)
+        self.frame4 = ttk.Frame(self.notebook)
+
+        # add the pages
+        self.notebook.add(self.frame0, text="Tutorial")
+        self.notebook.add(self.frame1, text="Step 1")
+        self.notebook.add(self.frame2, text="Step 2")
+        self.notebook.add(self.frame3, text="Step 3")
+        self.notebook.add(self.frame4, text="Step 4")
+
+
+class Notebook2:
+    def __init__(self, parent):
+        self.parent = parent
+
+        # top frame and attributes
+        self.top_frame = tkinter.Frame(parent, side="top", fill="x")
+        self.step1_lbl = tkinter.Label(self.top_frame, text="1.Open Inspect Element via the keyboard shortcut: 'F12' or"
+                                                            "'Ctrl + Shift + I'")
+
+
+        self.bottom_frame = tkinter.Frame(parent, side="top", fill="x")
+
+
+
+
